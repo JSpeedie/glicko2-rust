03d2a388c75b924f2b262c6c597b500f0ae8fc31
JSpeedie <julianspeedie@gmail.com>
2024-08-01 12:04:18 -0400

    Cleaned up and standardized documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![warn(missing_docs)]
 
-//! # Introduction
+//! # Preface
 //!
 //! To fully understand the functions contained in this library it will be
 //! necessary to understand the names Glicko-2 uses for various variables.
@@ -10,20 +10,20 @@
 //! 2. The player's rating deviation (RD)
 //! 3. The player's rating volatility (σ).
 //!
-//! Glicko-2 also uses 1 system constant (τ) in some of the calculations of
+//! Glicko-2 also uses one system constant (τ) in some of the calculations of
 //! the algorithm. What is important to note here is that two of these variables
-//! are stored in one scale or range, and used for calculations in another
-//! scale. The player's rating is stored in the "original scale" (i.e. the
-//! original Glicko scale) and is converted to the "Glicko-2 scale" for
+//! are stored in one scale or range, and then converted to another scale for
+//! calculations. The player's rating is stored in the "original scale" (i.e.
+//! the original Glicko scale) and is converted to the "Glicko-2 scale" for
 //! calculations. Likewise, the player's rating deviation is stored in the
 //! "original scale" and converted to the "Glicko-2 scale" for calculations.
 //! You can refer to the following table to understand which scale a given
 //! variable is on based on the name that is being used to refer to it:
 //!
-//! | Original Scale | Glicko-2 Scale |
-//! | -------------- | -------------- |
-//! | *r*            | μ              |
-//! | RD             | Φ              |
+//! | Variable         | Original Scale | Glicko-2 Scale |
+//! | ---------------- | -------------- | -------------- |
+//! | Rating           | *r*            | μ              |
+//! | Rating Deviation | RD             | Φ              |
 //!
 //! It should also be noted that the volatility (σ) does not change between
 //! scales and neither does the system constant (τ).
@@ -45,76 +45,79 @@ pub struct G2Player {
 }
 
 impl G2Player {
-    /// Returns the value of a given `G2Player`'s rating (μ) on the Glicko-2
-    /// scale. This function paired with the `get_rd()` function constitute
+    /// Returns the value of a given `G2Player`'s rating on the Glicko-2
+    /// scale (μ). This function paired with the `get_rd()` function constitute
     /// Step 2 of the Glicko-2 algorithm.
     ///
-    /// Parameters:
-    /// * 'self' a reference to the `G2Player` struct whose Glicko-2 scale
-    ///     rating will be returned.
+    /// ##### Parameters:
+    /// * `self` a reference to the `G2Player` struct whose Glicko-2 scale
+    ///     rating (μ) will be returned.
     ///
-    /// Return:
-    /// * An f64 representing the player's rating converted to the Glicko-2
-    ///     scale.
+    /// ##### Return:
+    /// * An `f64` representing the player's rating converted to the Glicko-2
+    ///     scale (μ).
     pub fn get_rating(&self) -> f64 {
         (self.rating * 173.7178) + 1500.0
     }
 
-    /// Sets the `G2Player`'s rating (*r*) on the original scale based on the
-    /// given Glicko-2 scale value `new_rating`. This function paired with the
-    /// `set_rd()` function consitute Step 8 of the Glicko-2 Algorithm.
+    /// Sets the `G2Player`'s rating on the original scale (*r*) based on the
+    /// given Glicko-2 scale value `new_rating` (μ). This function paired with
+    /// the `set_rd()` function consitute Step 8 of the Glicko-2 Algorithm.
     ///
-    /// Parameters:
+    /// ##### Parameters:
     /// * `self` a reference to the `G2Player` struct whose rating will be
     ///     changed.
-    /// * `new_rating` the Glicko-2 scale value to set `self`'s rating to.
+    /// * `new_rating` the Glicko-2 scale rating (μ) to set `self`'s original
+    ///     scale rating (*r*) to.
     ///
-    /// Return:
+    /// ##### Return:
     /// * Nothing - the function modifies the `G2Player` struct.
     pub fn set_rating(&mut self, new_rating: f64) {
         self.rating = (new_rating - 1500.0) / 173.7178;
     }
 
-    /// Returns the value of a given `G2Player`'s rating deviation (Φ) on the
-    /// Glicko-2 scale. This function paired with the `get_rating()` function
-    /// constitute Step 2 of the Glicko-2 algorithm.
+    /// Returns the value of a given `G2Player`'s rating deviation on the
+    /// Glicko-2 scale (Φ). This function paired with the `get_rating()`
+    /// function constitute Step 2 of the Glicko-2 algorithm.
     ///
-    /// Parameters:
-    /// * 'self' a reference to the `G2Player` struct whose Glicko-2 scale
-    ///     rating deviation will be returned.
+    /// ##### Parameters:
+    /// * `self` a reference to the `G2Player` struct whose Glicko-2 scale
+    ///     rating deviation (Φ) will be returned.
     ///
-    /// Return:
-    /// * An f64 representing the player's rating deviation converted to the
-    ///     Glicko-2 scale.
+    /// ##### Return:
+    /// * An `f64` representing the player's rating deviation converted to the
+    ///     Glicko-2 scale (Φ).
     pub fn get_rd(&self) -> f64 {
         self.rd * 173.7178
     }
 
-    /// Sets the `G2Player`'s rating deviation (RD) on the original scale based on
-    /// the given Glicko-2 scale value `new_rd`. This function paired with the
-    /// `set_rating()` function consitute Step 8 of the Glicko-2 Algorithm.
+    /// Sets the `G2Player`'s rating deviation on the original scale (RD) based
+    /// on the given Glicko-2 scale value `new_rd` (Φ). This function paired
+    /// with the `set_rating()` function consitute Step 8 of the Glicko-2
+    /// Algorithm.
     ///
-    /// Parameters:
+    /// ##### Parameters:
     /// * `self` a reference to the `G2Player` struct whose rating deviation
     ///     will be changed.
-    /// * 'new_rd' the Glicko-2 scale value to set `self`'s rating deviation to.
+    /// * `new_rd` the Glicko-2 scale value (Φ) to set `self`'s original scale
+    ///     rating deviation (RD) to.
     ///
-    /// Return:
+    /// ##### Return:
     /// * Nothing - the function modifies the `G2Player` struct.
     pub fn set_rd(&mut self, new_rd: f64) {
         self.rd = new_rd / 173.7178;
     }
 }
 
-/// The Glicko-2 *g* function used in Steps 3, 4 and 7.
+/// The Glicko-2 *g* function used in Steps 3, 4, 7, and by the *E* function.
 ///
-/// Parameters:
-/// * `phi` the Glicko-2 scale rating deviation of the player we need to call
-///     this function for.
-///
-/// Return:
-/// * The exact purpose or meaning of the value returned by this function is
-///     not made clear by the Glicko-2 PDF.
+/// #### Parameters:
+/// * `phi` the Glicko-2 scale rating deviation (Φ) of the player we need to
+///     call this function for.
+/// #### Return:
+/// * An `f64` produced by the *g* function of the Glicko-2 algorithm. The
+///     exact purpose or meaning of the value returned by this function is not
+///     made clear by the Glicko-2 PDF.
 pub fn _g(phi: f64) -> f64 {
 	1.0 / (1.0 + (3.0 * phi.powi(2) / PI.powi(2))).sqrt()
 }
@@ -122,17 +125,18 @@ pub fn _g(phi: f64) -> f64 {
 
 /// The Glicko-2 *E* function used in Steps 3, 4, and 7.
 ///
-/// Parameters:
-/// * `mu` the Glicko-2 scale rating of the player we need to call
-///     this function for.
-/// * `mu_j` the Glicko-2 scale rating of an opponent the player-of-interest
-///     played in the last rating period.
-/// * `phi_j` the Glicko-2 scale rating deviation of an opponent the
-///     player-of-interest played in the last rating period.
-///
-/// Return:
-/// * The exact purpose or meaning of the value returned by this function is
-///     not made clear by the Glicko-2 PDF.
+/// #### Parameters:
+/// * `mu` the Glicko-2 scale rating (μ) of the player we need to call this
+///     function for.
+/// * `mu_j` the Glicko-2 scale rating (μ<sub>j</sub>) of the player-of-interest's
+///     j<sup>th</sup> opponent played in the last rating period.
+/// * `phi_j` the Glicko-2 scale rating deviation (Φ<sub>j</sub>) of the
+///     player-of-interest's j<sup>th</sup> opponent played in the last rating
+///     period.
+/// #### Return:
+/// * An `f64` produced by the *E* function of the Glicko-2 algorithm. The
+///     exact purpose or meaning of the value returned by this function is not
+///     made clear by the Glicko-2 PDF.
 pub fn _E(mu: f64, mu_j: f64, phi_j: f64) -> f64 {
 	1.0 / (1.0 + E.powf(-1.0 * _g(phi_j) * (mu - mu_j)))
 }
@@ -142,17 +146,18 @@ pub fn _E(mu: f64, mu_j: f64, phi_j: f64) -> f64 {
 /// a player's rating based only on game outcomes. This function constitutes
 /// Step 3 of the Glicko-2 algorithm.
 ///
-/// Parameters:
+/// #### Parameters:
 /// * `mu` the Glicko-2 scale rating of the player we need to call
 ///     this function for.
-/// * `opp_ratings` an array of f64s containing (in order) all the ratings of
-///     the opponents `player` played.
-/// * `opp_rds` an array of f64s containing (in order) all the RDs of the
-///     opponents `player` played.
-///
-/// Return:
-/// * The v (*v*) quantity representing the estimated variance in
-///     a player's rating based only on game outcomes.
+/// * `opp_ratings` an array (μ<sub>1</sub>, ... , μ<sub>m</sub>) of `f64`s
+///     containing (in order) all the ratings of the opponents the
+///     player-of-interest played.
+/// * `opp_rds` an array (Φ<sub>1</sub>, ... , Φ<sub>m</sub>) of `f64`s
+///     containing (in order) all the rating deviations of the opponents
+///     the player-of-interest played.
+/// #### Return:
+/// * An `f64` representing the estimated variance in a player's rating (*v*)
+///     for a player who has played the given series of games.
 pub fn _v(mu: f64, opp_ratings: &[f64], opp_rds: &[f64]) -> f64 {
     let mut sum: f64 = 0.0;
 
@@ -171,22 +176,24 @@ pub fn _v(mu: f64, opp_ratings: &[f64], opp_rds: &[f64]) -> f64 {
 /// in rating based only on game outcomes. This function constitutes Step 4 of
 /// the Glicko-2 algorithm.
 ///
-/// Parameters:
-/// * `mu` the Glicko-2 scale rating of the player we need to call
-///     this function for.
-/// * `opp_ratings` an array of f64s containing (in order) all the ratings of
-///     the opponents `player` played.
-/// * `opp_rds` an array of f64s containing (in order) all the RDs of the
-///     opponents `player` played.
-/// * `scores` an array of f64s containing (in order) the scores
-///     of the games `player` played. 1 for `player` winning, 0.5 for a draw and
-///     0 for a `player` loss
+/// #### Parameters:
+/// * `mu` the Glicko-2 scale rating (μ) of the player we need to call this
+///     function for.
+/// * `opp_ratings` an array (μ<sub>1</sub>, ... , μ<sub>m</sub>) of `f64`s
+///     containing (in order) all the ratings of the opponents the
+///     player-of-interest played.
+/// * `opp_rds` an array (Φ<sub>1</sub>, ... , Φ<sub>m</sub>) of `f64`s
+///     containing (in order) all the rating deviations of the opponents
+///     the player-of-interest played.
+/// * `scores` an array (s<sub>1</sub>, ... , s<sub>m</sub>) of `f64`s
+///     containing (in order) the scores of the games the player-of-interest
+///     played. A value of 1 indicates that the player-of-interest won, 0.5
+///     indicates a draw, and 0 indicates that the player-of-interest lost.
 /// * `v` the quantity *v* calculated in Step 3 from this series of outcomes
 ///     representing the estimated variance of the player.
-///
-/// Return:
-/// * The estimated improvement in rating for a player who has the played
-///     the given series of games.
+/// #### Return:
+/// * An `f64` representing the estimated improvement in rating (∆) for a player
+///     who has the played the given series of games.
 pub fn delta(mu: f64, opp_ratings: &[f64], opp_rds: &[f64], scores: &[f64],
              v: f64) -> f64 {
 
@@ -201,20 +208,21 @@ pub fn delta(mu: f64, opp_ratings: &[f64], opp_rds: &[f64], scores: &[f64],
 
 
 /// Calculates the new volatility (σ') of the player with the rating deviation
-/// `phi` (Φ), and the volatility (σ) `sigma`, in a system with a system
+/// (Φ) `phi`, and the volatility (σ) `sigma`, in a system with a system
 /// constant (τ) `tau`, after a series of outcomes with a delta (∆) `delta` and
 /// a *v* `v`. This function constitutes Step 5 of the Glicko-2 algorithm.
 ///
-/// * `phi` the Glicko-2 scale value representing the player's rating deviation.
-/// * `sigma` the value representing the player's volatility.
-/// * `tau` the system constant.
+/// #### Parameters:
+/// * `phi` the Glicko-2 scale value representing the player's rating deviation
+///     (Φ).
+/// * `sigma` the value representing the player's volatility (σ).
+/// * `tau` the system constant (τ).
 /// * `v` the quantity *v* calculated in Step 3 from a series of outcomes
 ///     representing the estimated variance of the player.
 /// * `delta` the quantity ∆ calculated in Step 4 from a series of outcomes
 ///     representing the estimated improvement in rating of the player.
-///
-/// Return:
-/// * An f64 representing the player's new volatility (σ').
+/// #### Return:
+/// * An `f64` representing the player's new volatility (σ').
 pub fn new_vol(phi: f64, sigma: f64, tau: f64, v: f64, delta: f64) -> f64 {
     // Step 5, item 1
     let a: f64 = (sigma.powi(2)).ln();
@@ -270,19 +278,20 @@ pub fn new_vol(phi: f64, sigma: f64, tau: f64, v: f64, delta: f64) -> f64 {
 /// new pre-rating-period value. This function constitutes Step 6 of the
 /// Glicko-2 algorithm.
 ///
-/// This function can be used to increase the RD of players who did not compete
-/// in the rating period by providing their current rating deviation and
-/// current volatility (Φ, σ) instead of their current rating deviation
-/// and their new volatility (Φ, σ').
+/// This function can be used to increase the rating deviation of players who
+/// did not compete in the rating period by calling this function with their
+/// current rating deviation and current volatility (Φ, σ) instead of performing
+/// Steps 3-7 of the Glicko-2 algorithm. For reference, for players who did
+/// compete, this function is called in Step 6 with their current rating
+/// deviation and their new volatility (Φ, σ').
 ///
-/// Parameters:
-/// * `phi` the Glicko-2 scale rating deviation of the player we are performing
-///     this step of the algorithm for.
-/// * `new_sigma` the new volatility of the player we are performing this step
-///     of the algorithm for, calculated in Step 5.
-///
-/// Return:
-/// * An f64 representing the player's provisional rating deviation (Φ*)
+/// #### Parameters:
+/// * `phi` the Glicko-2 scale rating deviation (Φ) of the player we are
+///     performing this step of the algorithm for.
+/// * `new_sigma` the new volatility (σ') of the player we are performing this
+///     step of the algorithm for, calculated in Step 5.
+/// #### Return:
+/// * An `f64` representing the player's provisional rating deviation (Φ*)
 ///     calculated in Step 6.
 pub fn update_rd(phi: f64, new_sigma: f64) -> f64 {
     (phi.powi(2) + new_sigma.powi(2)).sqrt()
@@ -290,27 +299,31 @@ pub fn update_rd(phi: f64, new_sigma: f64) -> f64 {
 
 
 /// Returns an updated rating (μ') and an updated rating deviation (Φ') for a
-/// player that undergoes a series of outcomes with the given scores against
-/// opponents with the given ratings and RDs. This function constitutes Step 7
-/// of the Glicko-2 algorithm.
+/// player that undergoes a series of outcomes with scores
+/// (s<sub>1</sub>, ... , s<sub>m</sub>) against opponents with the ratings
+/// (μ<sub>1</sub>, ... , μ<sub>m</sub>) and rating deviations
+/// (Φ<sub>1</sub>, ... , Φ<sub>m</sub>). This function constitutes Step 7 of
+/// the Glicko-2 algorithm.
 ///
-/// Parameters:
-/// * `mu` the Glicko-2 scale rating of the player we need to call this
+/// #### Parameters:
+/// * `mu` the Glicko-2 scale rating (μ) of the player we need to call this
 ///     function for.
-/// * `prov_phi` the Glicko-2 scale provisional rating deviation of the player
-///     we are performing this step of the algorithm for. This value is
+/// * `prov_phi` the Glicko-2 scale provisional rating deviation (Φ*) of the
+///     player we are performing this step of the algorithm for. This value is
 ///     calculated by the `update_rd()` function aka. Step 6.
-/// * 'opp_ratings' an array of f64s containing (in order) all the ratings of
-///     the opponents `player` played.
-/// * 'opp_rds' an array of f64s containing (in order) all the RDs of the
-///     opponents `player` played.
-/// * 'scores' an array of f32s containing (in order) the scores
-///     of the games `player` played. 1 for `player` winning, 0.5 for a draw and
-///     0 for a `player` loss
-///
-/// Return:
-/// * A tuple containing the player's new rating and RD on the Glicko-2 scale
-///     (Φ', μ')
+/// * `opp_ratings` an array (μ<sub>1</sub>, ... , μ<sub>m</sub>) of `f64`s
+///     containing (in order) all the ratings of the opponents the
+///     player-of-interest played.
+/// * `opp_rds` an array (Φ<sub>1</sub>, ... , Φ<sub>m</sub>) of `f64`s
+///     containing (in order) all the rating deviations of the opponents
+///     the player-of-interest played.
+/// * `scores` an array (s<sub>1</sub>, ... , s<sub>m</sub>) of `f64`s
+///     containing (in order) the scores of the games the player-of-interest
+///     played. A value of 1 indicates that the player-of-interest won, 0.5
+///     indicates a draw, and 0 indicates that the player-of-interest lost.
+/// #### Return:
+/// * An `(f64, f64)` tuple containing the player-of-interest's new rating and
+///     rating deviation on the Glicko-2 scale (Φ', μ').
 pub fn update_player(mu: f64, prov_phi: f64, opp_ratings: &[f64],
                      opp_rds: &[f64], scores: &[f64], v: f64) -> (f64, f64) {
 
